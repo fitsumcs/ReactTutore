a338a0cf47126b986abc2567d2579c42a990e93b
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-07 19:46:57 +0300

    adding expression using jsx

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import TodoContainer from "./App.js"
 
 const element = <h1>Hello from Create React App</h1>
 
+//expression 
+
+const myelement = <h1>React is {5 + 5} times better with JSX</h1>;
+
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+
+ReactDOM.render(myelement, document.getElementById("root1"))
+
