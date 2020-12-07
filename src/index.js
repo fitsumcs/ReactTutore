0ef5c26a819bbc36ec1624caa7edf064e858b7d4
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-07 19:38:16 +0300

    Adding componenet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+//component file
+import TodoContainer from "./App.js"
+
 const element = <h1>Hello from Create React App</h1>
 
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
